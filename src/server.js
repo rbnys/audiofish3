39ec9d756cc33d3b118191ca0cf639776d610f31
uyuyuy99 <uyuyuy99@gmail.com>
2026-08-31 19:21:38 -0700

    Frontend now has configurable port & backend URL

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
-const serverIP = process.env.NODE_ENV === 'production' ? 'http://audiofish.org:4001' : 'http://localhost:4001';
+const serverPort = process.env.PORT || 4001;
+const backendURL = process.env.BACKEND_URL || `https://audiofish.onrender.com:${serverPort}`;
+const serverIP = process.env.NODE_ENV === 'production' ? backendURL : `http://localhost:${serverPort}`;
 
 export default serverIP;
