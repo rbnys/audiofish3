d68caf45f0f976a342b0f1b2edd7a23cff95af7a
uyuyuy99 <uyuyuy99@gmail.com>
2026-08-31 21:50:19 -0700

    Updated default backend ports

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
-const serverPort = process.env.PORT || 4001;
+const serverPort = process.env.PORT || 10000;
 const backendURL = process.env.BACKEND_URL || `https://audiofish.onrender.com:${serverPort}`;
-const serverIP = process.env.NODE_ENV === 'production' ? backendURL : `http://localhost:${serverPort}`;
+const serverIP = process.env.NODE_ENV === 'production' ? backendURL : `http://localhost:4001`;
 
 export default serverIP;
